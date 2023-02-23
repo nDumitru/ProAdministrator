f54a035805e1b03cfb954d37584a0a9091428659
nDumitru <nechitadumitru@gmail.com>
2023-02-23 19:47:07 +0200

    2023/02/23_19:47

--- residents/urls.py
+++ residents/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
+from . import views
 
-from residents import views
+app_name = 'residents'
 
 urlpatterns = [
-    path('create_resident/', views.ResidentCreateView.as_view(), name='create-residents'),
-    path('list_of_residents/', views.ResidentListView.as_view(), name='list-of-residents'),
-    path('update_resident/<int:pk>/', views.ResidentUpdateView.as_view(), name='update-residents'),
-    path('delete_resident/<int:pk>/', views.ResidentDeleteView.as_view(), name='delete-residents'),
-    path('details_of_resident/<int:pk>/', views.ResidentDetailView.as_view(), name='details-of-residents'),
-    path('inactive_resident/<int:pk>/', views.inactivate_resident, name='inactive-residents'),
-
+    path('', views.index, name='index'),
+    path('resident/<int:pk>/', views.ResidentDetailView.as_view(), name='resident_detail'),
+    path('resident/add/', views.ResidentCreateView.as_view(), name='resident_add'),
+    path('resident/<int:pk>/update/', views.ResidentUpdateView.as_view(), name='resident_update'),
+    path('resident/<int:pk>/delete/', views.ResidentDeleteView.as_view(), name='resident_delete'),
 ]
