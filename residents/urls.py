65c0761cdd64fc6167f800b04a6a255aa34e7a62
nDumitru <nechitadumitru@gmail.com>
2023-02-24 02:33:48 +0200

    2023/02/24_02:24

--- residents/urls.py
+++ residents/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from . import views
+from residents import views
+from .views import ResidentListView
 
 app_name = 'residents'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('', ResidentListView.as_view(), name='resident-list'),
     path('resident/<int:pk>/', views.ResidentDetailView.as_view(), name='resident_detail'),
     path('resident/add/', views.ResidentCreateView.as_view(), name='resident_add'),
     path('resident/<int:pk>/update/', views.ResidentUpdateView.as_view(), name='resident_update'),
